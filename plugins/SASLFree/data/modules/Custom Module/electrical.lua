10045b2ad5ad45c2c672cb4620cf52b5bb91da54
Joseph Frazer <frazer380@gmail.com>
2020-08-06 05:46:23 -0400

    Rewrite of the Electrical System
    Generators now supply the AC system
    
    Battery switch now turns on both batteries
    
    If the batteries are on, the emergency bus gets powered
    
    Once the emergency bus is powered, the DC Essential Bus is powered
    
    DC Cross Tie now works. It is automatically set, but using a push button can be selected to off
    
    Dual DC Bus is now functional (provides power for lift dumpers, anti-skid, and speed brakes in the event of a DC Bus 1 or DC Bus 2 failure during landing)
    
    GCU is now simulated, so when the generator push buttons are powered (including the APU one), the generators are set to on
    
    GPCU is now simulates, so when the GPU is aval, and the push button is set to the correct position, the AC Ground Service Bus will be powered
    
    The system automatically powers the AC Emergency Bus if the AC Essential Bus is powered
    
    The system automatically powers the TRU if the correct AC Bus is powered. For TRU 1 and TRU 2, the push buttons are required to be on (they are set to be automatically)
    
    The system automatically powers the DC Busses if the correct TRU is powered

--- plugins/SASLFree/data/modules/Custom Module/electrical.lua
+++ plugins/SASLFree/data/modules/Custom Module/electrical.lua
@@ -1,190 +1,142 @@
-local yellow	= {1, 1, 0, 1}
+-- CREATED BY: JOE
+-- CREATED ON: 8/5/2020
 
-local function RGB(r,g,b,a) return { r/255, g/255, b/255, a and a/255 or 1} end
 
-white = RGB(256, 256, 256)
-green = RGB(17, 90, 34)
-blue = RGB(62, 107, 99)
-local font1	= loadFont(getXPlanePath() .. "Resources/fonts/ProFontWindows.ttf")
-
-local onGround = globalProperty("sim/flightmodel/failures/onground_any")
-
--- Create two batteries
-local battery1 = createGlobalPropertyi("fokker/electrical/battery1", 0)
-local battery1Charging = createGlobalPropertyi("fokker/electrical/battery1_charging", 0)
-local battery2 = createGlobalPropertyi("fokker/electrical/battery2", 0)
-local battery2Charging = createGlobalPropertyi("fokker/electrical/battery2_charging", 0)
-
--- APU
-local apu = createGlobalPropertyi("fokker/electrical/apu", 0) -- APU (ON/OFF)
-local apu_gen = createGlobalPropertyi("fokker/electrical/apu_gen", 0) -- APU GENERATOR (ON/OFF)
---local apu_gen_pb = createGlobalPropertyi("fokker/electrical/apu_gen_pb", 0) -- APU GEN PUSH BUTTON
-
--- GPU
-local gpu = createGlobalPropertyi("fokker/electrical/gpu", 0)
-local gpu_gen = createGlobalPropertyi("fokker/electrical/gpu_gen", 0)
-local gpu_connected = createGlobalPropertyi("fokker/external/gpu_connected", 0)
-
--- Engines
-local gen1 = createGlobalPropertyi("fokker/electrical/eng1_gen", 0) -- Engine 1 generator
-local gen2 = createGlobalPropertyi("fokker/electrical/gen2_gen", 0) -- Engine 2 generator
-
--- Control
-local battery1ChargingControl = createGlobalPropertyi("fokker/electrical/batt1ChargCtrl", 0) -- 0 is off, therefore the batteries WONT be charged EVEN if the SUFFICENT AC current is PROVIDED (USED FOR THE OVERHEAD CONTROL SWITCH)
-local battery2ChargingControl = createGlobalPropertyi("fokker/electrical/batt2ChargCtrl", 0) -- 0 is off, therefore the batteries WONT be charged EVEN if the SUFFICENT AC current is PROVIDED (USED FOR THE OVERHEAD CONTROL SWITCH)
-
+-- Generators
+---- Engines
+local gen1 = createGlobalPropertyi("fokker/electrical/generator_1", 0) -- Supplies AC BUS 1 AND the ESS BUS
+local gen2 = createGlobalPropertyi("fokker/electrical/generator_2", 0) -- Supplies AC BUS 2
+----  APU
+local apu_on = createGlobalPropertyi("fokker/electrical/gpu_on", 0)
+local apu_gen = createGlobalPropertyi("fokker/electrical/apu_gen", 0)
+---- External Power
+local gpu_is_aval = createGlobalPropertyi("fokker/electrical/gpu_is_aval", 0)
+local gpu_pwr = createGlobalPropertyi("fokker/electrical/gpu_pwr", 0)
 -- Busses
-local dcBus1 = createGlobalPropertyi("fokker/electrical/dcBus1", 0) -- Create dc bus 1
-local acBus1 = createGlobalPropertyi("fokker/electrical/acBus1", 0) -- Create ac bus 1 | CONNECTED TO: Gen 1, APU gen
-local acBus2 = createGlobalPropertyi("fokker/electrical/asBus2", 0) -- Create ac bus 2
-local essAcBus = createGlobalPropertyi("fokker/electrical/essAcBus", 0) 
-
+local acBus1 = createGlobalPropertyi("fokker/electrical/acBus1", 0)
+local acBus2 = createGlobalPropertyi("fokker/electrical/acBus2", 0)
+local acEssBus = createGlobalPropertyi("fokker/electrical/acEssBus", 0)
+local acEmerBus = createGlobalPropertyi("fokker/electrical/acEmerBus", 0)
+local acGndSerBus = createGlobalPropertyi("fokker/electrical/acGndSerBus", 0)
 -- TRU
-tru1 = createGlobalPropertyi("fokker/electrical/tru1", 1) -- Create the TRU
-esstru = createGlobalPropertyi("fokker/electrical/esstru", 1) -- Create the essential bus TRU
-
--- Voltages
-local batt1Voltage = createGlobalPropertyi("fokker/electrical/battery1_voltage", 0)
-local batt2Voltage = createGlobalPropertyi("fokker/electrical/battery2_voltage", 0)
-local dcBus1Voltage = createGlobalPropertyi("fokker/electrical/dcBus1Voltage", 0)
-local acBus1Voltage = createGlobalPropertyi("fokker/electrical/acBus1Voltage", 0)
-local acBus2Voltage = createGlobalPropertyi("fokker/electrical/acBus2Voltage", 0)
-
-local essAcBusVoltage = createGlobalPropertyi("fokker/electrical/essAcBusVoltage", 0)
-
-local bus1OnBat = false
-local bus1OnTRU = false
-
-local truFail = false -- IF THIS IS TRUE THAT MEANS THE TRU HAS FAILED (IF FAILED LOGIC WILL TURN IF OFF AUTOMATICALLY)
-local truOverheat = true -- TRU Overheat
-local truRevCrnt = false -- TRU Reverse Current
-
-local acPoweredByAPU = false
-local acPoweredByExternalPower = false
-local acPoweredByBothEngineGens = false
-local acPoweredByGen1 = false
-local acPoweredByGen2 = false
-
-
+local tru1 = createGlobalPropertyi("fokker/electrical/tru1", 0) -- Powered by AC BUS 1
+local tru2 = createGlobalPropertyi("fokker/electrical/tru2", 0) -- Powered by AC BUS 2
+local truEss = createGlobalPropertyi("fokker/electrical/truEss", 0) -- Powered  by ESS BUS
+local truEmer = createGlobalPropertyi("fokker/electrical/truEmer", 0)
+local truGndSer = createGlobalPropertyi("fokker/electrical/truGndSer", 0) -- Powered by AC GND SER BUS
+-- Busses
+local dcBus1 = createGlobalPropertyi("fokker/electrical/dcBus1", 0)
+local dcBus2 = createGlobalPropertyi("fokker/electrical/dcBus2", 0)
+local dcEssBus = createGlobalPropertyi("fokker/electrical/dcEssBus", 0)
+local dcEmerBus = createGlobalPropertyi("fokker/electrical/dcEmerBus", 0)
+local dcGndSerBus = createGlobalPropertyi("fokker/electrical/dcGndSerBus", 0)
+local dualDcBus = createGlobalPropertyi("fokker/electrical/dualDCBus", 0)
+-- Cross Ties
+local dcb1_dbc2_x_tie = createGlobalPropertyi("fokker/electrical/dcb1_dbc2_x_tie", 0)
+-- Batteries
+local batt1 = createGlobalPropertyi("fokker/electrical/battery_1", 0)
+local batt2 = createGlobalPropertyi("fokker/electrical/battery_2", 0)
+-- Push Buttons
+local battery_switch = createGlobalPropertyi("fokker/buttons/battery_switch", 0)
+local gpu_pb = createGlobalPropertyi("fokker/buttons/gpu_pb", 0)
+local ess_emer_pwr_only = createGlobalPropertyi("fokker/buttons/ess_emer_pwr_only", 0)
+local gen1_pb = createGlobalPropertyi("fokker/buttons/gen1_pb", 0)
+local gen2_pb = createGlobalPropertyi("fokker/buttons/gen2_pb", 0)
+local apu_gen_pb = createGlobalPropertyi("fokker/buttons/apu_gen_pb", 0)
+local dcX_Tie_pb = createGlobalPropertyi("fokker/buttons/dcX_Tie_pb", 1)
+local acX_Tie_pb1 = createGlobalPropertyi("fokker/buttons/acX_Tie_pb1", 1)
+local acX_Tie_pb2 = createGlobalPropertyi("fokker/buttons/acX_Tie_pb1", 1)
+local tru1_pb = createGlobalPropertyi("fokker/buttons/tru1_pb", 1)
+local tru2_pb = createGlobalPropertyi("fokker/buttons/tru2_pb", 1)
+local onGround = globalProperty("sim/flightmodel/failures/onground_any")
 function update()
-
-    -- Set voltages
-    if get(battery1) == 1 then  -- If the battery 1 is on
-        set(batt1Voltage, 28)
+    if get(dcEssBus) == 0 then 
+        -- PRESENT ALERT WHEN ESS DC BUS 
     end
+    -- GENERATORS
+    if get(gen1) == 1 and get(gen2) == 1 then -- IF GEN 1 AND GEN 2 ARE ON
+        set(acBus1, 1)
+        set(acEssBus, 1)
+        set(acBus2, 1)
+    elseif get(apu_gen) == 1 and get(acGndSerBus) == 1 then
+            -- LET APU POWER THE ESSENTIAL AC BUS
+            set(acEssBus, 1)
+            -- LET GPU POWER AC BUS 1 AND AC BUS 2
+            set(acBus1, 1)
+            set(acBus2, 1)
 
-    -- Turn on/off the AC BUSSES depending on which systems are alive
-    -- AC BUS 1 is powered from: 
-    -- ENGINE GEN 1 / ENGINE GEN 2
-    -- APU GEN
-    -- EXTERNAL POWER
-    -- IF ANY OF THESE FAIL, THE AC BUS WILL AUTOMATICALLY SWITCH TO THE NEXT ACTIVE POWER SOURCE
-    --------------
-    -- AC POWER IS DESTRIBUTED TO:
-    -- AC BUSSES 1 AND 2
-    -- ESS AC BUS
-    -- EMER AC BUS
-    -- GALLEY BUS 1 AND 2
-    -- AC GROUND SERVICES BUS
-
-
-    -- APU
-    -- On ground, with only APU pwr aval, entire elec sys is energized [DONE]
-    -- IF one engine gen is inoperative, the APU will supply the engine generators busses [DONE]
-    -- The essential bus will be supplied by the active engine generator [DONE]
-    -- The essential bus is normally powered by gen 1, but if it becomes inoperative it's supplied by gen 2 [DONE]
-    -- IF BOTH ENGINES INOPERATIVE, the APU GEN supplies the AC busses [DONE]
-
-    function powerEssAcBus()
-        set(essAcBus, 1) -- SET THE ESSENTIAL AC BUS TO ON
-        set(essAcBusVoltage, 115)
-        if get(esstru) == 1 then -- IF THE ESSENTIAL AC BUS TRU IS ON, CONVERT ESS AC BUS AC CURRENT TO DC
-            
-        end
-    end
-    
-    function powerAc()
-        set(acBus1, 1) -- Set the AC BUS 1 TO ON
-        set(acBus1Voltage, 115) -- Set the AC BUS 1 Voltage to 115
-        set(acBus2, 1) --  Set the AC BUS 2 TO ON
-        set(acBus2Voltage, 115) -- Set the AC BUS 2 Voltage to 115
-    end
-
-    if get(gen1) == 1 and get(gen2) == 1 then -- IF ENGINE GEN 1 AND 2 IS OPERATIVE
-        acPoweredByBothEngineGens = true
-        acPoweredByAPU = false
-        acPoweredByExternalPower = false
-        powerAc()
-        powerEssAcBus()
-    elseif get(gen1) == 0 and get(gen2) == 1 then -- IF GEN 1 ISN'T ACTIVE, POWER THE ESSENTIAL BUS THRU GEN 2
-        acPoweredByGen2 = true
-        powerEssAcBus()
-    elseif get(gen2) == 0 and get(gen1) == 1 then -- IF GEN 1 IS ACTIVE BUT GEN 2 ISNT
-        acPoweredByGen2 = true
-    elseif get(gen1)  == 0 and get(gen2) == 0 then
-        acPoweredByBothEngineGens = 0
-    end
-    
-    -- SIMULATE GPCU (Ground Power Control Unit)
-    if get(gpu_gen) == 1 then -- If the External power generator is on
-        if get(gpu) == 1 and get(gpu_connected) == 1 then -- If the GPU is ON and CONNECTED
-            acPoweredByExternalPower = true
-            acPoweredByAPU = false
-            powerAc()
+    elseif get(apu_gen) == 1 then -- ACTS AS STANDBY IF GEN 1 AND GEN 2 ARE ON | REMEMBER: APU_GEN IS ONLY ON IF THE APU IS ON AND THE GENERATOR PUSH BUTTON (PB) IS ON
+        if get(onGround) == 1 then
+            if get(gen1) == 0 and get(gen2) == 0 then -- ON GROUND, WHEN BOTH ENGINES ARE OFF, AND APU  IS ON, POWER ALL OF THE AC SYSTEM
+               set(acBus1, 1)
+               set(acBus2, 1)
+            end 
         else
-            acPoweredByExternalPower = false
-            -- MESSAGE ECAM THAT GPU IS OFF BUT GEN IS ON?
-        end
-    end
-
-    if get(apu_gen) == 1 then  -- If the gpu generator is off and the apu generator is on (controlled by overhead push button) (meaning allowing generated power from the APU)
-        if get(apu) == 1 then -- If the APU is on
-            if get(onGround) == 1 and not acPoweredByBothEngineGens and not acPoweredByExternalPower then -- If the aircraft is on the ground, power the entire electrical system IF engine generators are off AND if the externak power generator is off
-                acPoweredByAPU = true
-                powerAc()
-            elseif get(onGround) == 0 and get(gen1) == 0 and get(gen2) == 0 then --  IF both engines inoperative, power both ac busses
-                acPoweredByAPU = true
-                powerAc()
+            if get(gen1) == 0 and get(gen2) == 0 then -- IF BOTH ENGINES ARE FAILED
+                set(acBus1, 1)
+                set(acBus2, 1)
+            elseif get(gen1) == 1 and get(gen2) == 0 then -- IF GEN 1 FAILS
+                set(acBus1, 1)
+            elseif get(gen1) == 0 and get(gen2) == 1 then -- IF GEN 2 FAILS
+                set(acBus2, 1)
             end
         end
+    elseif get(acGndSerBus) == 1 then
+        set(acBus1, 1)
+        set(acEssBus, 1)
+        set(acBus2, 1)
     end
-
-    
-
-
-    -- Automatically supply DC Bus 1 power
-    if get(acBus1) == 1 and get(tru1) == 1 then    -- If AC bus is powered and the TRU is on (controlled by push button on overhead)
-        set(dcBus1, 1) -- Supply power to the DC BUS
-        if(get(battery1ChargingControl) == 1) then  -- If the overhead button ALLOWS batt charging for BATT 1
-            set(battery1Charging, 1) -- Charge battery 1
-        end
-        if(get(battery2ChargingControl) == 1) then  -- If the overhead button ALLOWS batt charging for BATT 2
-            set(battery2Charging, 1) -- Charge battery 2
-        end
-        bus1OnBat = false -- Set on bat to false, since bus 1 is getting power from the ac bus (converted via the TRU)
-        bus1OnTRU = true -- Set to true because bus 1 is powered via the AC current coming from the TRU
-    elseif get(acBus1) == 0 then -- If the ac bus isn't powered
-        if get(battery1) == 1 then -- But the battery bus IS powered
-            set(dcBus1, 1) -- Supplying power to the DC bus
-            set(battery1Charging, 0) -- Not charging battery 1
-            set(battery2Charging, 0) -- Not charging battery 2
-            bus1OnBat = true -- Set on bat to true, since bus 1 is getting power from battery 1
-            bus1OnTRU = false
-        elseif get(battery1) == 0 then -- But the battery bus ISN'T powered
-            set(dcBus1, 0) -- Not supplying power to the DC bus
-            set(battery1Charging, 0) -- Not charging battery 1
-            set(battery2Charging, 0) -- Not charging battery 2
-            bus1OnBat = false -- DC Bus 1 is NOT on battery power
-            bus1OnTRU = false
+    -- If the battery switch is on
+    if get(battery_switch) == 1 then
+        set(batt1, 1)
+        set(batt2, 2)
+    end
+    -- If batteries are on, power the emergency bus
+    if get(batt1) == 1 or get(batt2) == 1 then
+        set(dcEmerBus, 1)
+    end
+    -- Once the emergency bus is powered, power the DC Essential Bus, the DC BUS 1, and IF THE CROSS TIE IS ON (WHICH IT IS AUTOMATICALLY), TURN THE DC BUS 2 ON
+    -- Basically simulating the DC Essential Bus and DC X-TIE
+    if get(dcEmerBus) == 1 then 
+        set(dcEssBus, 1)
+        set(dcBus1, 1)
+        if get(dcX_Tie_pb) == 1 then
+            set(dcBus2, 1)
+        else
+            set(dcBus2, 0)
         end
     end
-
-    -- Unless there is a failure, the DC BUS 1 VOLTAGE IS 28 V (we can calculate realistic voltage once we have actual battery data)
-    if bus1OnBat then -- If the DC BUS 1 IS ON BATTERY POWER, then SET the BUS to 28 V
-       set(dcBus1Voltage, 28)
-    elseif not bus1OnBat and bus1OnTRU then -- If the DC BUS 1 electrical power is supplied from the TRU, set the bus to 28 V
-        set(dcBus1Voltage, 28)
-    else
-        set(dcBus1Voltage, 0) -- Else, if no power is supplied, the BUS voltage is nothing
+    if get(dcBus1) == 1 or get(dcBus2) == 1 then set(dualDcBus, 1) end -- DUAL DC BUS IS UN-INTERRUPTED | PROVIDES ELECTRICAL POWER FOR lift dumpers, anti-skid, speed brakes IF DC BUS 1 OR DC BUS 2 FAILS DURING LANDING
+    -- SIMULATE THE GCU
+    if get(gen1_pb) == 1 then set(gen1, 1) else set(gen1, 0) end
+    if get(gen2_pb) == 1 then set(gen2, 1) else set(gen2, 0) end
+    if get(apu_gen_pb) == 1 and get(apu_on) == 1 then set(apu_gen, 1) else set(apu_gen, 0) end
+    -- SIMULATE THE GPCU
+    if get(gpu_is_aval) == 1 then
+        if get(gpu_pb) == 1 then
+            set(acGndSerBus, 1) -- If the GPU is on, and the push button is on, then supply power to the AC Ground Service Bus
+            ------ TODO: MEMO MESSAGE, EXT POWER CONNECTED (PRESENTED AT THE MFDU PRIMARY PAGE)
+        else
+            ------ TODO: MEMO MESSAGE, EXT POWER CONNECTED (PRESENTED AT THE MFDU PRIMARY PAGE)
+        end
     end
+    -- Automatically power the AC Emergency Bus
+    if get(acEssBus) == 1 then 
+        set(acEmerBus, 1) -- IF THE AC ESS BUS IS ON, POWER THE EMER BUS
+    elseif get(batt1) == 1 and get(batt2) == 1 then
+        set(acEmerBus, 1) -- HAVE THE BATTERIES CONTINUE TO POWER THE AC EMER BUS
+        --------------------- TODO: In this case, an AC SUPPLY alert will be presented at the Standby Annunciator Panel (SAP); See section Flight Warnign System. TOTAL LOSS OF EMERGENCY POWER MAY OCCUR AFTER 30 MINUTES, BUT IS ACTUALLY DEPENDANT ON THE BATTERY CONFIGURATION AND SYSTEM LOADS
+    else set(acEmerBus, 0) end
+    -- If the AC busses are powered, turn the TRU on, else turn them off
+    if get(acBus1) == 1 and get(tru1_pb) == 1 then set(tru1, 1) else set(tru1, 0) end
+    if get(acBus2) == 1 and get(tru2_pb) == 1 then set(tru2, 1)  else set(tru2, 0) end
+    if get(acEssBus) == 1 then set(truEss, 1) else set(truEss, 0) end
+    if get(acEmerBus) == 1 then set(truEmer, 1) else set(truEmer, 0) end
+    if get(acGndSerBus) == 1 then set(truGndSer, 1) else set(truGndSer, 0) end
+    -- AND IF the TRU's are powered, the coresponding DC busses will then be powered
+    if get(tru1) == 1 then set(dcBus1, 1) else set(dcBus1, 0) end
+    if get(tru2) == 1 then set(dcBus2, 1) else set(dcBus2, 0) end
+    if get(truEss) == 1 then set(dcEssBus, 1) else set(dcEssBus, 0) end
+    if get(truEmer) == 1 then set(dcEmerBus, 1) else set(dcEmerBus, 0) end
+    if get(truGndSer) == 1 then set(dcGndSerBus, 1) else set(dcGndSerBus, 0) end
 end
